22c59a6e1938d8eeff0c492071d713655eefaf0b
Shrivu Shankar <shrivu1122@gmail.com>
2020-06-04 19:51:47 -0500

    🕹️: Email Sending

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 let spawn = require('child_process').spawn;
-
-// ffmpeg -i test.flv -c:v libx264 -crf 19 -strict experimental test.mp4
+const Email = require('email-templates');
+const nodemailer = require('nodemailer');
+const nodemailerSendgrid = require('nodemailer-sendgrid');
+const { v4: uuidv4 } = require('uuid');
 
 const port = process.env.PORT || process.env.WEB_PORT || 5000;
 const host = process.env.WEB_HOST || '0.0.0.0';
@@ -11,26 +13,59 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const transport = nodemailer.createTransport(
+  nodemailerSendgrid({
+    apiKey: process.env.SG_KEY
+  })
+);
+
 app.use('/', express.static(path.join(__dirname, 'build')));
 
+let sendVideo = async (emailAddr, videoFn) => {
+  const email = new Email({
+    message: {
+      from: 'noreply@sshh.io',
+      attachments: [
+        {
+          filename: 'recording.mp4',
+          path: videoFn
+        }
+      ]
+    },
+    preview: false,
+    send: true,
+    transport: transport
+  });
+  await email
+    .send({
+      template: 'video',
+      message: {
+        to: emailAddr
+      },
+      locals: {}
+    });
+};
+
 io.on('connection', (sock) => {
+  const id = uuidv4();
   console.log('connected!');
   let sockState = {
     video: false,
     ffmpeg: null
   };
-  sock.emit('hb:client');
+  sock.emit('start', JSON.stringify({ id: id }));
   sock.on('start', (data) => {
+    Object.assign(sockState, JSON.parse(data));
     sockState.video = true;
     let ops = [
-			'-i','-',
-			'-c:v', 'libx264', '-preset', 'veryfast', '-tune', 'zerolatency',
-			'-c:a', 'aac', '-ar', '44100', '-b:a', '64k',
-			'-y',
-			'-use_wallclock_as_timestamps', '1',
-			'-async', '1', 
-			'-bufsize', '1000',
-			'-f', 'flv', 'test.flv'
+      '-i', '-',
+      '-c:v', 'libx264', '-preset', 'veryfast', '-tune', 'zerolatency',
+      '-c:a', 'aac', '-ar', '44100', '-b:a', '64k',
+      '-y',
+      '-use_wallclock_as_timestamps', '1',
+      '-async', '1',
+      '-bufsize', '1000',
+      '-f', 'flv', 'vidtmp/' + id + '.flv'
     ];
     console.log('STARTING');
     sockState.ffmpeg = spawn('ffmpeg', ops);
@@ -48,14 +83,17 @@ io.on('connection', (sock) => {
     if (sockState.video) {
       sockState.video = false;
       sockState.ffmpeg.kill('SIGINT');
+      let cvt = spawn('ffmpeg', ['-i', 'vidtmp/' + id + '.flv',
+        '-c:v', 'libx264', '-crf', '19', '-strict',
+        'experimental', '-y', 'vidtmp/' + id + '.mp4']);
+      cvt.on('error', (err) => {
+        console.error(err);
+      });
+      cvt.on('exit', (err) => {
+        let { email } = sockState;
+        sendVideo(email, 'vidtmp/' + id + '.mp4');
+      });
     }
-    let cvt = spawn('ffmpeg', ['-i', 'test.flv', '-c:v', 'libx264', '-crf', '19', '-strict', 'experimental', '-y', 'test.mp4']);
-    cvt.on('error', (err) => {
-      console.error(err);
-    });
-    cvt.on('end', (err) => {
-      console.log('CVT DONE');
-    });
   }
   sock.on('stop', () => {
     endStream();
--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import React, { Component } from 'react';
 import * as S from 'semantic-ui-react'
 import './App.css';
 
+const CFG_KEY = 'cfgv1';
+
 class App extends Component {
 
   constructor(props) {
     super(props);
+    let cfg;
+    if (!localStorage.getItem(CFG_KEY)) {
+      cfg = {
+        name: null,
+        email: null
+      };
+    } else {
+      cfg = JSON.parse(localStorage.getItem(CFG_KEY))
+    }
     this.state = {
       streaming: false,
-      sock: false
+      sock: false,
+      settings: !cfg.name,
+      name: cfg.name,
+      email: cfg.email
     }
     this.playerRef = React.createRef();
   }
 
   componentDidMount() {
     this.setupSocket();
+    this.saveCfg();
+  }
+
+  saveCfg() {
+    let { name, email } = this.state;
+    localStorage.setItem(CFG_KEY, JSON.stringify({
+      name, email
+    }));
   }
 
   setupSocket() {
-    this.socket = window.io.connect('ws://' + window.location.host, { secure: false });
-    this.socket.on('hb:client', () => {
+    let secure = ('https:' == window.location.protocol);
+    let sockProto = (secure ? 'wss://' : 'ws://');
+    this.socket = window.io.connect(sockProto + window.location.host, { secure: secure });
+    this.socket.on('start', () => {
       this.setState({ sock: true });
     });
     this.socket.on('disconnect', () => {
@@ -28,13 +52,9 @@ class App extends Component {
   }
 
   async startStream() {
-    let [width, height] = [window.innerWidth, window.innerHeight];
     let constraints = {
       audio: true,
-      video: {
-        width: { min: width / 20, ideal: width, max: width * 20 },
-        height: { min: height / 20, ideal: height, max: height * 20 },
-      }
+      video: { facingMode: "environment" }
     };
     try {
       this.stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -47,7 +67,7 @@ class App extends Component {
       videoPlayer.play();
       this.setState({ streaming: true });
     };
-    this.socket.emit('start');
+    this.socket.emit('start', localStorage.getItem(CFG_KEY));
     this.recorder = new MediaRecorder(this.stream);
     this.recorder.start(0);
     this.recorder.ondataavailable = (evt) => {
@@ -63,24 +83,44 @@ class App extends Component {
   }
 
   render() {
-    let { streaming, sock } = this.state;
+    let { streaming, sock, settings } = this.state;
     return (
       <div className="App">
-        {sock ? 
+        {sock ?
           <S.Label style={styles.connLabel} as='a' content='Connected' icon='wifi' /> :
           <S.Label style={styles.connOffLabel} as='a' content='Offline' icon='wifi' />}
-        <video style={{display: streaming ? 'block': 'hidden'}} muted={true} ref={this.playerRef}></video>
-        {!streaming && <S.Button 
+        <video style={{ display: streaming ? 'block' : 'hidden' }} muted={true} ref={this.playerRef}></video>
+        {!streaming && <S.Button
           style={styles.bigCenterBtn} circular
           disabled={!sock}
           onClick={() => this.startStream()}
           color='red' size='massive' icon='record'></S.Button>}
-        {streaming && <S.Button 
+        {streaming && <S.Button
           style={styles.bigBottomBtn} circular
           disabled={!sock}
           onClick={() => this.stopStream()}
           color='yellow' size='massive' icon='stop'></S.Button>}
-      </div >
+        <S.Modal open={settings}>
+          <S.Modal.Content>
+            <S.Modal.Description>
+              <S.Form>
+                <S.Form.Field>
+                  <label>Name</label>
+                  <input onChange={(evt) => this.setState({ name: evt.target.value })} placeholder='' />
+                </S.Form.Field>
+                <S.Form.Field>
+                  <label>Email</label>
+                  <input onChange={(evt) => this.setState({ email: evt.target.value })} placeholder='' />
+                </S.Form.Field>
+                <S.Button color='blue' onClick={() => {
+                  this.setState({ settings: false });
+                  this.saveCfg();
+                }} type='submit'>Save</S.Button>
+              </S.Form>
+            </S.Modal.Description>
+          </S.Modal.Content>
+        </S.Modal>
+      </div>
     );
   }
 
@@ -91,14 +131,14 @@ const styles = {
     zIndex: 99, position: 'absolute',
     top: '50%', left: '50%', marginRight: '-50%', transform: 'translate(-50%, -50%)',
     fontSize: '3rem'
-  }, 
+  },
   bigBottomBtn: {
     zIndex: 99, position: 'absolute',
     bottom: '5%', left: '50%', marginRight: '-50%', transform: 'translate(-50%, -50%)',
     fontSize: '3rem'
   },
-  connLabel: {zIndex: 99, position: 'absolute', backgroundColor: 'unset', color: 'green'},
-  connOffLabel: {zIndex: 99, position: 'absolute', backgroundColor: 'unset', color: 'red'}
+  connLabel: { zIndex: 99, position: 'absolute', backgroundColor: 'unset', color: 'green' },
+  connOffLabel: { zIndex: 99, position: 'absolute', backgroundColor: 'unset', color: 'red' }
 };
 
 export default App;
